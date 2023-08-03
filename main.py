afc62d4e8ff1c9c07beaa5954a97068ba86ecc5d
bartek <bartex600700@gmail.com>
2023-08-03 02:17:46 +0200

    display posts from api and changed bg images

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
+import requests
+
+posts_data = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def get_home():
-    return render_template("index.html")
+    return render_template("index.html", posts_data=posts_data)
 
 
 @app.route("/about")
