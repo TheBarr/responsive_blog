f0e974dc650ca4158cac9a1901448228673c6055
bartek <bartex600700@gmail.com>
2023-08-07 23:14:05 +0200

    contact form

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 
 posts_data = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
@@ -15,9 +15,18 @@ def get_about():
     return render_template("about.html")
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def get_contact():
-    return render_template("contact.html")
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+        message = request.form['message']
+        print(name, email, phone, message)
+        return "<h1> Successfully sent your message."
+    else:
+        return render_template("contact.html")
+
 
 @app.route("/post/<int:id>")
 def get_post(id):
@@ -26,7 +35,6 @@ def get_post(id):
         if post["id"] == id:
             requested_post = post
     return render_template("post.html", post=requested_post)
-
 
 
 if __name__ == "__main__":
